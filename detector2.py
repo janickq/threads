8833e6e6ab8eb7587d963d37ff6e5f3dcb0308c4
janickq <carlchu0701@gmail.com>
2022-05-09 14:36:06 +0800

    detector somewhat working

--- detector2.py
+++ detector2.py
@@ -47,6 +47,48 @@ class detector2:
             self.labels = [line.strip() for line in f.readlines()]
         
 
-        def run(image):
-            pass
+    def run(self, image):
+
+        # image = cv2.imread(image_path)
+        image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        imH, imW, _ = image.shape 
+        image_resized = cv2.resize(image_rgb, (self.width, self.height))
+        input_data = np.expand_dims(image_resized, axis=0)
+
+        # Normalize pixel values if using a floating model (i.e. if model is non-quantized)
+        if self.floating_model:
+            input_data = (np.float32(input_data) - self.input_mean) / self.input_std
+
+        # Perform the actual detection by running the model with the image as input
+        self.interpreter.set_tensor(self.input_details[0]['index'], input_data)
+        self.interpreter.invoke()
+
+        # Retrieve detection results
+        boxes = self.interpreter.get_tensor(self.output_details[self.boxes_idx]['index'])[0] # Bounding box coordinates of detected objects
+        classes = self.interpreter.get_tensor(self.output_details[self.classes_idx]['index'])[0] # Class index of detected objects
+        scores = self.interpreter.get_tensor(self.output_details[self.scores_idx]['index'])[0] # Confidence of detected objects
+
+        # Loop over all detections and draw detection box if confidence is above minimum threshold
+        for i in range(len(scores)):
+            if ((scores[i] > 0.7) and (scores[i] <= 1.0)):
+
+                # Get bounding box coordinates and draw box
+                # Interpreter can return coordinates that are outside of image dimensions, need to force them to be within image using max() and min()
+                ymin = int(max(1,(boxes[i][0] * imH)))
+                xmin = int(max(1,(boxes[i][1] * imW)))
+                ymax = int(min(imH,(boxes[i][2] * imH)))
+                xmax = int(min(imW,(boxes[i][3] * imW)))
+                
+                cv2.rectangle(image, (xmin,ymin), (xmax,ymax), (10, 255, 0), 2)
+
+                # Draw label
+                object_name = self.labels[int(classes[i])] # Look up object name from "labels" array using class index
+                label = '%s: %d%%' % (object_name, int(scores[i]*100)) # Example: 'person: 72%'
+                labelSize, baseLine = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 0.7, 2) # Get font size
+                label_ymin = max(ymin, labelSize[1] + 10) # Make sure not to draw label too close to top of window
+                cv2.rectangle(image, (xmin, label_ymin-labelSize[1]-10), (xmin+labelSize[0], label_ymin+baseLine-10), (255, 255, 255), cv2.FILLED) # Draw white box to put label text in
+                cv2.putText(image, label, (xmin, label_ymin-7), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 0), 2) # Draw label text
+
+        # All the results have been drawn on the image, now display the image
+        return image
 
--- main.py
+++ main.py
@@ -3,8 +3,12 @@ from threading import Thread
 from comms import comms
 from time import sleep
 from commands import commands
+from detector2 import detector2
+from cam import cam
+import cv2
 
-
+stream = cam()
+detector = detector2("models/model.tflite", "models/labels.txt")
 comm = comms()
 cmd = commands()
 m_flag = False
@@ -27,13 +31,43 @@ def command_thread(args):#commands
     pass
 
 
-def comms_thread(ags):
+def comms_thread(args):
     global m_flag
     while True:
         if m_flag:
             print("comm_thread active")
             sleep(1)
     
+def detector_thread(args):
+    global m_flag
+    while True:
+        if m_flag:
+            img = stream.read()
+            # cv2.imshow('img', img)
+            # cv2.waitKey(1)
+            detected = detector.run(img)
+            print("detector running")
+            cv2.imshow("detector", detected)
+            cv2.waitKey(1)
+            sleep(1)
+            
+        else:
+            print("cam offline")
+            sleep(1)
+    
+# def cam_thread(args):
+#     global m_flag
+#     while True:
+#         if m_flag:
+#             print("cam_thread active")
+#             stream.update()
+#             img = stream.read()
+#             cv2.imshow("stream", img)
+#             # detector(stream.read)
+#             cv2.waitKey(1)
+        
+            
+
     
     
 
@@ -41,10 +75,14 @@ if __name__ == "__main__":
     masterthread = Thread(target = master, args = (10, ))
     commsthread = Thread(target = comms_thread, args = (10, ))
     cmdthread = Thread(target = command_thread, args = (10, ))
+    detectorthread = Thread(target = detector_thread, args = (10, ))
     masterthread.start()
+    detectorthread.start()
     commsthread.start()
-    masterthread.join()
-    masterthread.join()
     print("thread started")
+    masterthread.join()
+    camthread.join()
+    commsthread.join()
+    
     
     
